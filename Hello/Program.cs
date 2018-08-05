20bd41152b1548d533ea476707f1431af7e953f5
Ewa Kozłowska <ewa.kozlowska1233@gmail.com>
2018-08-05 16:10:35 +0200

    Pierwszy program Hello. Tablica args_2

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Hello
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            string name1 = args[1];
+            string name2 = args[2];
+
+            Console.WriteLine("witaj, " + name + "\nwitaj, " + name1 + "\nwitaj, " + name2);
+
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+
+            //}
         }
     }
 }
